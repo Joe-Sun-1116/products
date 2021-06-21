4aa03c9f48aec2090d9d771d97ba48f65cc804e2
Joe Sun <a0927554266@gmail.com>
2021-06-22 00:06:26 +0800

    add new func

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
 products = []
+
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+print(products)
+
 while True:
 	name = input('Please enter name of products.')
 	if name == 'q':
@@ -6,7 +16,7 @@ while True:
 	price = input('Please enter the price.')
 	products.append([name, price])
 
-with open('products.txt', 'w', encoding='utf-8') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
